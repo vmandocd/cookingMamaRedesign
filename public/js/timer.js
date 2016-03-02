3d767c8dc96326f600fee9b91b5f184de31578cc
wscottmiller <wmiller@ucsd.edu>
2016-03-02 23:56:06 +0000

    timer functionality, updated add recipe

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,17 +1,22 @@
+var tickAudio = new Audio("http://freesound.org/data/previews/137/137227_1735491-lq.mp3");
+
 var defaults = {}
   , one_second = 1000
   , one_minute = one_second * 60
   , one_hour = one_minute * 60
   , one_day = one_hour * 24
   , startDate = new Date()
-  , face = document.getElementById('lazy');
+  , face = document.getElementById('lazy')
+  ;
 
+  //$('input').bootstrapNumber();
+var sec;
 
 var imgs = document.querySelectorAll('.handspan');
 
 for ( var i = 0; i < imgs.length; i++ ) {
-    imgs[i].onclick = toggleAnimation;  
-    imgs[i].style.webkitAnimationPlayState = 'running';  
+    imgs[i].onclick = toggleAnimation;
+    imgs[i].style.webkitAnimationPlayState = 'running';
 }
 
 function toggleAnimation() {
@@ -21,17 +26,40 @@ function toggleAnimation() {
         if ( style.webkitAnimationPlayState === 'running' ) {
             style.webkitAnimationPlayState = 'paused';
             document.body.className = 'paused';
+            console.log('toggle paused' + i);
+
         } else {
             style.webkitAnimationPlayState = 'running';
-            document.body.className = '';       
+            document.body.className = '';
+            console.log('toggle running' + i);
         }
-    }      
+    }
 }
 
 toggleAnimation();
 
 function start() {
 
+  hour = document.getElementById("hour").value;
+
+  if(hour < 10){
+    if(hour == ''){
+      hour = '00';
+    }else{
+      hour = '0'+ hour;
+    }
+  }
+  console.log("hours: " + hour);
+  sec = document.getElementById("sec").value;
+
+  if(sec < 10){
+    if(sec == ''){
+      sec = '00';
+    }else{
+      sec = '0'+ sec;
+    }
+  }
+  console.log("seconds: " + sec);
   toggleAnimation();
 
   if(timerIsOn) {
@@ -47,13 +75,13 @@ function start() {
     theInterval = window.setInterval(tick, 250);
     timerIsOn = true;
   }
-
 }
 
 startDate = false;
 var thePauseTime = false;
 var timerIsOn = false;
 var theInterval;
+var timerNotify = false;
 
 function tick() {
 
@@ -68,5 +96,16 @@ function tick() {
   parts[1] = (parts[1].length == 1) ? '0' + parts[1] : parts[1];
   parts[2] = (parts[2].length == 1) ? '0' + parts[2] : parts[2];
 
-  face.innerText = parts.join(':');  
+
+
+  if(parts[1] == hour && parts[2] == sec){
+    tickAudio.play();
+    start();
+    if(timerNotify == false){
+      $('#notificationBlock').notification('Timer is done!', {stickMode: true});
+      timerNotify = true;
+    }
+  }
+
+  face.innerText = parts.join(':');
 }
